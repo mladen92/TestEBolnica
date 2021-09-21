d8281f9f4fe77457ea2223a3ccb057fe576161c8
LazarM <mladenmakilazarevic@gmail.com>
2021-09-20 20:57:25 -0400

    add  scenario za dodaj radnika

---
+++ src/test/java/utilities/Utility.java
@@ -0,0 +1,11 @@
+package utilities;
+
+public class Utility {
+
+    // param String in format mm/dd/yyyy
+    // need String in format yyyy-mm-dd
+   public String getDate(String date){
+
+    }
+
+}
